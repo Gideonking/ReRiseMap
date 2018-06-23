b1d1b45e4bd8fcc23f72d3db57ffd6fdc09fb5fc
mdiannna <mdiannna@yahoo.com>
2018-06-23 20:11:53 +0300

    added links

--- resources/views/buildings_map.blade.php
+++ resources/views/buildings_map.blade.php
@@ -1,80 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>ReRiseMap</title>
-
-  <!-- Bootstrap Core CSS -->
-  <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-
-  <!-- Fonts -->
-  <link href="{{ asset('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="{{ asset('css/nivo-lightbox.css')}}" rel="stylesheet" />
-  <link href="{{ asset('css/nivo-lightbox-theme/default/default.css')}}" rel="stylesheet" type="text/css" />
-  <link href="{{ asset('css/animate.css')}}" rel="stylesheet" />
-  <!-- Squad theme CSS -->
-  <link href="{{ asset('css/style.css')}}" rel="stylesheet">
-  <link href="{{ asset('color/default.css')}}" rel="stylesheet">
+@section('content')
   <link href="{{ asset('css/map.css')}}" rel="stylesheet">
-  <!-- =======================================================
-    Theme Name: Ninestars
-    Theme URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
-    Author: BootstrapMade
-    Author URL: https://bootstrapmade.com
-  ======================================================= -->
-</head>
 
-<body data-spy="scroll">
-
-  <div class="container">
-    <ul id="gn-menu" class="gn-menu-main">
-      <li class="gn-trigger">
-        <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
-        <nav class="gn-menu-wrapper">
-          <div class="gn-scroller">
-            <ul class="gn-menu">
-              <li class="gn-search-item">
-                <input placeholder="Search" type="search" class="gn-search">
-                <a class="gn-icon gn-icon-search"><span>Search</span></a>
-              </li>
-              <li>
-                <a href="#about" class="gn-icon gn-icon-download">Despre</a>
-              </li>
-              <li><a href="#clasa1" class="gn-icon gn-icon-help">Clasa I de risc seismic</a></li>
-              <li><a href="#clasa2" class="gn-icon gn-icon-help">Clasa a II-a de risc seismic</a></li>
-              <li><a href="#clasa3" class="gn-icon gn-icon-help">Clasa a III-a de risc seismic</a></li>
-              <li>
-                <a href="#contact" class="gn-icon gn-icon-archive">Contact</a>
-              </li>
-            </ul>
-          </div>
-          <!-- /gn-scroller -->
-        </nav>
-      </li>
-      <li><a href="/">ReRise</a></li>
-      <li><b>Linie urgenta Re-Rise : 0434 432 234</b></li>
-      <li class="hidden-xs">
-        <ul class="company-social">
-          <li class="social-facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-          <li class="social-google"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
 
   <section class="home-section text-center">
     <div>
-      <h1>Map</h1>
+      <h1>ReRiseMap</h1>
       
       <div class="pac-card" id="pac-card">
       <div>
         <div id="title">
-          Autocomplete search
+          Adauga locatia ta
         </div>
         <div id="type-selector" class="pac-controls">
           <input type="radio" name="type" id="changetype-all" checked="checked">
@@ -109,63 +46,6 @@
     </div>
   </section>
   
-  <footer>
-    <div class="container">
-
-          <div class="row">
-            <div class="col-lg-12">
-              <address>
-                <strong>@ReRise.org</strong><br>
-                  <a href="http://rerise.org">http://rerise.org </a><br>
-                  <abbr title="Phone">P:</abbr> 0434 432 234
-                  <br>
-                  <br>
-                  <strong>Email</strong><br>
-
-                <a href="mailto:#">office@rerise.org</a><br/>
-              </address>
-            </div>
-
-      <div class="row">
-        <div class="col-md-12 col-lg-12">
-          <p>&copy; FSociety</p>
-          <div class="credits">
-            <!-- 
-              All the links in the footer should remain intact. 
-              You can delete the links only if you purchased the pro version.
-              Licensing information: https://bootstrapmade.com/license/
-              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Ninestars
-            -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-
-
-  <!-- Core JavaScript Files -->
-  <!-- <script src="{{asset('js/jquery.min.js')}}"></script> -->
-  <script
-        src="https://code.jquery.com/jquery-3.3.1.js"
-        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-        crossorigin="anonymous"></script>
-    
-
-  <script src="{{asset('js/bootstrap.min.js')}}"></script>
-  <script src="{{asset('js/jquery.easing.min.js')}}"></script>
-  <script src="{{asset('js/classie.js')}}"></script>
-  <script src="{{asset('js/gnmenu.js')}}"></script>
-  <script src="{{asset('js/jquery.scrollTo.js')}}"></script>
-  <script src="{{asset('js/nivo-lightbox.min.js')}}"></script>
-  <script src="{{asset('js/stellar.js')}}"></script>
-  <!-- Custom Theme JavaScript -->
-  <script src="{{asset('js/custom.js')}}"></script>
-  <script src="{{asset('contactform/contactform.js')}}"></script>
-
-
  <script>
       var markers = [];
 
@@ -292,6 +172,7 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuUyRkHWpdoe6MTege8frZbNco9cRNP1c&libraries=places&callback=initMap" async defer></script>
-</body>
 
-</html>
+
+
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>ReRiseMap</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+
+  <!-- Fonts -->
+  <link href="{{ asset('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/nivo-lightbox.css')}}" rel="stylesheet" />
+  <link href="{{ asset('css/nivo-lightbox-theme/default/default.css')}}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('css/animate.css')}}" rel="stylesheet" />
+  <!-- Squad theme CSS -->
+  <link href="{{ asset('css/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('color/default.css')}}" rel="stylesheet">
+  <!-- =======================================================
+    Theme Name: Ninestars
+    Theme URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+</head>
+
+<body data-spy="scroll">
+
+  <div class="container">
+    <ul id="gn-menu" class="gn-menu-main">
+      <li class="gn-trigger">
+        <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+        <nav class="gn-menu-wrapper">
+          <div class="gn-scroller">
+            <ul class="gn-menu">
+              <li class="gn-search-item">
+                <input placeholder="Search" type="search" class="gn-search">
+                <a class="gn-icon gn-icon-search"><span>Search</span></a>
+              </li>
+              <li>
+                <a href="/#about" class="gn-icon gn-icon-download">Despre</a>
+              </li>
+              <li>
+                <a href="/buildings-map" class="gn-icon gn-icon-download">Harta</a>
+              </li>
+              <li><a href="#clasa1" class="gn-icon gn-icon-help">Clasa I de risc seismic</a></li>
+              <li><a href="#clasa2" class="gn-icon gn-icon-help">Clasa a II-a de risc seismic</a></li>
+              <li><a href="#clasa3" class="gn-icon gn-icon-help">Clasa a III-a de risc seismic</a></li>
+              <li>
+                <a href="#contact" class="gn-icon gn-icon-archive">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /gn-scroller -->
+        </nav>
+      </li>
+      <li><a href="/">ReRise</a></li>
+      <li><b>Linie urgenta Re-Rise : 0434 432 234</b></li>
+      <li class="hidden-xs">
+        <ul class="company-social">
+          <li class="social-facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          <li class="social-google"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+
+   @yield('content')
+
+  <footer>
+    <div class="container">
+
+          <div class="row">
+            <div class="col-lg-12">
+              <address>
+                <strong>@ReRise.org</strong><br>
+                  <a href="http://rerise.org">http://rerise.org </a><br>
+                  <abbr title="Phone">P:</abbr> 0434 432 234
+                  <br>
+                  <br>
+                  <strong>Email</strong><br>
+
+                <a href="mailto:#">office@rerise.org</a><br/>
+              </address>
+            </div>
+
+      <div class="row">
+        <div class="col-md-12 col-lg-12">
+          <p>&copy; FSociety</p>
+          <div class="credits">
+            <!-- 
+              All the links in the footer should remain intact. 
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Ninestars
+            -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Core JavaScript Files -->
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('js/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('js/classie.js')}}"></script>
+  <script src="{{asset('js/gnmenu.js')}}"></script>
+  <script src="{{asset('js/jquery.scrollTo.js')}}"></script>
+  <script src="{{asset('js/nivo-lightbox.min.js')}}"></script>
+  <script src="{{asset('js/stellar.js')}}"></script>
+  <!-- Custom Theme JavaScript -->
+  <script src="{{asset('js/custom.js')}}"></script>
+  <script src="{{asset('contactform/contactform.js')}}"></script>
+
+</body>
+
+</html>
